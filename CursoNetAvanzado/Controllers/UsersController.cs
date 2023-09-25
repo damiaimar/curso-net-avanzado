71442f3abee6a3b7d4e43488385e44dff3639fde
Aimar Damian <damian.aimar@cablasociados.com>
2023-09-25 11:27:17 -0300

    Agrego controlador UsersController

---
+++ CursoNetAvanzado/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using CursoNetAvanzado.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoNetAvanzado.Controllers
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        [Route("getuser")]
+        public IActionResult Get()
+        {
+            var user = new UserModel()
+            {
+                Nombre = "Damián",
+                Apellido = "Aimar",
+                Documento = "37257944"
+            };
+
+            return View("Index", user);
+        }
+
+		[HttpGet]
+		[Route("create")]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        [Route("create")]
+        public IActionResult Create(UserModel model)
+        {
+			return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CursoNetAvanzado/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoNetAvanzado.Models
+{
+    public class UserModel
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+		public string? Documento { get; set; }
+
+	}
+}
