f2cc9b0fd2c28af289f150f6aa4cdb27ef0eeae5
Aimar Damian <damian.aimar@cablasociados.com>
2023-09-25 09:24:05 -0300

    code commit

---
+++ CursoNetAvanzado/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CursoNetAvanzado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
